24d29463fa8dce4458b9ca058268180702e45410
NielsTRS <40001070+NielsTRS@users.noreply.github.com>
2020-09-04 20:52:26 +0200

    Reformat file

--- scrap.py
+++ scrap.py
@@ -1,8 +1,10 @@
-from json import dump,load
+from json import dump, load
 from feedparser import parse
 
+
 def get_new_articles():
-    links = {"articles": [x["link"].split("https://www.lemondedupc.fr")[1] for x in parse("https://www.lemondedupc.fr/rss.xml").entries]}
+    links = {"articles": [x["link"].split("https://www.lemondedupc.fr")[1] for x in
+                          parse("https://www.lemondedupc.fr/rss.xml").entries]}
     logs = load(open('logs.json'))
 
     for link in links["articles"]:
