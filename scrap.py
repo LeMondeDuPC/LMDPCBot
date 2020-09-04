c1c51e6288433351fb94801e12acb39ef0b9e0a7
NielsTRS <40001070+NielsTRS@users.noreply.github.com>
2020-09-04 21:42:29 +0200

    Fix path

--- scrap.py
+++ scrap.py
@@ -5,13 +5,13 @@ from feedparser import parse
 def get_new_articles():
     links = {"articles": [x["link"].split("https://www.lemondedupc.fr")[1] for x in
                           parse("https://www.lemondedupc.fr/rss.xml").entries]}
-    logs = load(open('logs.json'))
+    logs = load(open('./logs.json'))
 
     for link in links["articles"]:
         if link not in logs["articles"]:
             newlogs = logs
             newlogs["articles"].insert(0, link)
-            dump(newlogs, open('logs.json', 'w'), indent=4)
+            dump(newlogs, open('./logs.json', 'w'), indent=4)
             return link
         else:
             return ""
